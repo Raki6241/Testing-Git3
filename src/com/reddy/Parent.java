a915ae0755fde4bc653139e81b21344d38b5bda1
Rakesh <Rakesh@192.168.1.39>
2020-02-23 00:46:48 +0530

    Testing

---
+++ src/com/reddy/Parent.java
@@ -0,0 +1,20 @@
+package com.reddy;
+
+ class Parent { 
+	 public void raki(){
+		System.out.println("hello welcome");
+		System.out.println("goodnight1");
+	}	
+	
+	public static void main(String[] args) {
+		Parent p= new Parent();
+	    p.raki();
+
+		
+	
+		System.out.println("goodnight2");
+}
+ }
+ 
+ 
+
